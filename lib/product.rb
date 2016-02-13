2ca8cf2bc314f000a6ebd6ceac81618082fd2e5d
fresh502 <fresh502@gmail.com>
2016-02-13 17:40:05 +0900

    Add ability to find products with name, stock

--- lib/product.rb
+++ lib/product.rb
@@ -1,15 +1,33 @@
 class Product
-	attr_reader :title
+	attr_reader :title, :price, :stock
 
 	@@products = []
 
 	def initialize(options={})
 		@title = options[:title]
+		@price = options[:price]
+		@stock = options[:stock]
 		add_to_products(options[:title])
 	end
 
 	def self.all
 		@@products
+	end
+
+	def self.find_by_title(title)
+		@@products.each do |product|
+			if product.title == title
+				break product
+			end
+		end
+	end
+
+	def in_stock?
+		@stock > 0 ? true : false
+	end
+
+	def self.in_stock
+		@@products.select { |product| product.stock > 0 }
 	end
 
 	private
