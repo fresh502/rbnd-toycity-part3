455cd6073ff1fcb0a705f0e5d2bf4ce628bf5b0b
fresh502 <fresh502@gmail.com>
2016-02-13 17:01:04 +0900

    Add DuplicateProductError

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,27 @@
 class Product
+	attr_reader :title
+
 	@@products = []
 
 	def initialize(options={})
-		@@products <<  self
+		@title = options[:title]
+		add_to_products(options[:title])
 	end
 
 	def self.all
 		@@products
+	end
+
+	private
+	def add_to_products(title)
+		is_new_product = true
+		@@products.each do |product|
+			if product.title == title
+				is_new_product = false
+				raise DuplicateProductError, "#{title} already exists"
+				break
+			end
+		end
+		@@products << self if is_new_product
 	end
 end
